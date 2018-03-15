11f5044383b1dba2aabc0bfba324e9fc165764cc
juliensaad <saadjulien@gmail.com>
2018-03-14 21:26:58 -0400

    Remove all iPad specific changes on iPhone

--- Classes/YTPlayerView.m
+++ Classes/YTPlayerView.m
@@ -858,13 +858,13 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
         frame = CGRectMake(0.0, 0.0, 4096.0, 2160.0);
     }
     else {
-        frame = CGRectZero;
+        frame = self.bounds;
     }
     UIWebView *webView = [[UIWebView alloc] initWithFrame:frame];
-    webView.translatesAutoresizingMaskIntoConstraints = NO;
-    webView.scalesPageToFit = YES;
     webView.scrollView.scrollEnabled = NO;
     webView.scrollView.bounces = NO;
+    webView.backgroundColor = UIColor.blackColor;
+    webView.scrollView.backgroundColor = UIColor.blackColor;
 
     [self updateWebViewFrame:webView];
 
@@ -880,14 +880,17 @@ shouldStartLoadWithRequest:(NSURLRequest *)request
 
 - (void)updateWebViewFrame:(UIWebView *)webView {
     if (UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad) {
+        webView.translatesAutoresizingMaskIntoConstraints = NO;
         CGSize contentSize = webView.scrollView.contentSize;
         CGSize viewSize = UIScreen.mainScreen.bounds.size;
         float scale = viewSize.width / contentSize.width;
         webView.scrollView.minimumZoomScale = scale;
         webView.scrollView.maximumZoomScale = scale;
         webView.scrollView.zoomScale = scale;
-        webView.backgroundColor = UIColor.blackColor;
-        webView.scrollView.backgroundColor = UIColor.blackColor;
+    }
+    else {
+        webView.translatesAutoresizingMaskIntoConstraints = YES;
+        webView.autoresizingMask = (UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight);
     }
 }
 
